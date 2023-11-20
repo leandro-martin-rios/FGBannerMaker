b91dc9524f61b1ffc29412ca9477fa5bd0912baa
Leandro Martin Rios <leaxplosion@gmail.com>
2023-11-19 21:07:35 -0300

    Select created, json created

--- scripts.js
+++ scripts.js
@@ -1,14 +1,42 @@
-document.getElementById("changeColor").addEventListener("input", cambio);
+// LECTURA DE JSON
 
-function cambio() {
-    const bannerBackgroundColor = document.getElementById("changeColor").value;
-    document.querySelector(".banner-container").style.backgroundColor = `#${bannerBackgroundColor}`;
+import data from './data.json' assert { type: 'json' };
+console.log(data.DBFZ.Characters.length);
+
+// SELECCION DE PERSONAJE
+
+const charactersSelect = document.getElementById("characters");
+
+for(let i = 0; i < data.DBFZ.Characters.length; i++) {
+    let option = document.createElement("option");
+    option.className = "showChar";
+    option.value = data.DBFZ.Characters[i].Name;
+    option.text = data.DBFZ.Characters[i].Name;
+    if(i == 0) {
+        option.selected = "selected";
+    }
+    charactersSelect.appendChild(option);
 }
+
+charactersSelect.addEventListener('change', function() {
+    document.querySelector('.banner-render').src = `./img/renders/${charactersSelect.value}.png`;
+})
+
+
+// CAMBIO DE COLOR EN BANNER
+
+document.getElementById("changeColor").addEventListener("input", changeGradientColor);
+
+function changeGradientColor() {
+    const bannerBackgroundColor = document.getElementById("changeColor").value;
+    document.querySelector(".banner-container").style.background = `linear-gradient(#000000, ${bannerBackgroundColor})`;
+}
+
+// DESCARGA DE IMAGEN
 
 document.querySelector('#btnImg').addEventListener('click', function() {
     html2canvas(document.querySelector('#banner'), {
       onrendered: function(canvas) {
-        // document.body.appendChild(canvas);
         return Canvas2Image.saveAsPNG(canvas);
       }
     });
